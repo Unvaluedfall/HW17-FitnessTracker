1a2c07daa40a31b5adb33a62569496f7f0292fd0
Jordon <Jordonrunge20@gmail.com>
2020-04-28 21:38:36 -0500

    Set up heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var mongoose = require("mongoose");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
-
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 
 app.use(express.static("public"));
@@ -15,10 +15,7 @@ app.use(express.json());
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(MONGODB_URI);
 
 app.listen(PORT, function() {
   console.log(`Now listening on port: ${PORT}`);
